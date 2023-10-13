38629b06b5a04914605b2cf7fc60b3fdf32a59c8
Bryny <28590441+BrynyPatchett@users.noreply.github.com>
2023-10-13 16:34:48 +1300

    Added basic JS

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Test");
+
+const search = document.querySelector('#searchbar');
+const searchButton = document.querySelector('.searchicon');
+
+
+searchButton.addEventListener("click",function(e){
+    console.log(search.value);
+});
